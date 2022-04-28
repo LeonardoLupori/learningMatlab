1309e910c3f17f8528d9ad51f4e26c962c8805d7
Leonardo Lupori <leonardo.lupori@gmail.com>
2022-04-28 13:35:57 +0200

    fixed a bugin solution.m

--- 01_simpleSignalAnalysis/solution.m
+++ 01_simpleSignalAnalysis/solution.m
@@ -1,4 +1,4 @@
-a = createSignal;
+signal = createSignal;
 t = 0:10:(10000-10);
 
 % Create the figure
@@ -6,26 +6,31 @@ f = figure('Color',[.9, .9 , 1]);
 
 % First Subplot
 axs(1) = subplot(2,2,1);
-plot(t,a,'DisplayName','raw')
+plot(t,signal,'DisplayName','raw')
 
 % Second Subplot
 axs(2) = subplot(2,2,2);
-plot(t,smooth(a,20),'r', 'DisplayName','filtered')
-hold(axs(2),'off')
-plot(t,smooth(a,20),'r', 'DisplayName','filtered')
-
+plot(t,smooth(signal,10),'r', 'DisplayName','filtered')
+hold(axs(2),'on')
+filtered = smooth(signal,20);
+[~, maxIdx] = max(filtered);
+trend = filtered - detrend(filtered, 1, maxIdx);
+plot(t,trend,'g', 'DisplayName','Trend')
 
 hold(axs(2),'off')
 
 % Third Subplot
 axs(3) = subplot(2,2,[3,4]);
+plot(t(1:maxIdx), signal(1:maxIdx),'b','DisplayName','ascending')
+hold(axs(3),'on')
+plot(t(maxIdx+1:end), signal(maxIdx+1:end),'r','DisplayName','descending')
+hold(axs(3),'off')
 
-
-
-
+% Customize all the axes
+titles = {'1^{st} Plot','2^{nd} Plot','3^{rd} Plot'};
 for i = 1:length(axs)
     axs(i).XLabel.String = 'Time(ms)';
     axs(i).YLabel.String = 'Signal Amplitude';
-    legend(axs(i))
-
+    legend(axs(i),'location','best')
+    title(axs(i), titles{i})
 end
